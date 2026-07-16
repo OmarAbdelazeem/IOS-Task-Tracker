6c46bf8f750bcfd48b04eca792f295baa6983ca4
Omar Abdelazeem <omar.abdelazeem87@gmail.com>
2026-07-16 16:32:17 +0300

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Omar Abdelazeem on 16/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
